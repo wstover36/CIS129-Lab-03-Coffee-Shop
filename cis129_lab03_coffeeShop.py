180cb235f5bb4cf76c70165802b9d9007981e1db
wstover36 <157067489+wstover36@users.noreply.github.com>
2024-02-29 15:55:15 -0700

    Update cis129_lab03_coffeeShop.py
    Reworked entire code, removed several menus items and replaced with Americanos, Donuts, and Redbull.

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -1,28 +1,50 @@
+#Coffee Shop Simulator
 #Wolfgang C. Stover
-#CIS129
+#02/29/2024
 
-print("*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@")
-print("*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@")
+#Greating for Coffee Shop Simulator
+print("Welcome to Wolfgang's Coffee Shop!")
+ 
+#Constants
 
-print("Welcome to Wolfgang C. Stover's Coffee and Muffin Shop!")
+priceOfAmericano = 1
+priceOfDonut = 1
+priceOfRedBull = 2
 
-print("*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@")
-print("*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@")
+#tax
 
-priceAmericano = 2.30
-priceDirtyChaiTea = 3.00
-priceDonut = 1.00
-priceDozenDonuts = 10
+tax = 0.06
 
-totalPrice = priceAmericano + priceDirtyChaiTea
+nbrOfAmericano = int(input('Number of Americano(s) bought: '))
+nbrOfDonut  = int(input('Number of donut(s) bought: '))
+nbrOfRedBull = int(input('Number of Red Bull(s) bought: '))
 
-tax = 0.09
+#SubTotal Calculations
 
-totalPriceWithTax = totalPrice + tax
+subTotalAmericano = nbrOfAmericano * priceOfAmericano
+subTotalDonut  = nbrOfDonut  * priceOfDonut
+subTotalRedBull = nbrOfRedBull  * priceOfRedBull
 
-nbrOfDonuts = int(input("How many donuts would you like to buy?"))
-nbrOfAmericano = int(input("How many Americanos would you like to buy"))
-nbrOfDirtyChaiTea = int(input("How many dirty chai tea would you like to buy"))
+#Secondary Tax calculation for Total
 
-subTotal = (nbrOfDonuts * priceDonut)
-subTotal                           
+totalNoTax = subTotalAmericano + subTotalDonut + subTotalRedBull
+taxAmount = (totalNoTax * tax)
+grandTotal = totalNoTax * tax + totalNoTax
+
+#Receipt
+
+print("*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*")
+
+print("Thank you for visiting my Shop! I hope to see you again soon.")
+
+print(str(nbrOfAmericano) + " Americano(s) @ $1 each: $ " + str(subTotalAmericano))
+print(str(nbrOfDonut) + " Donut(s) @ $1 each: $ " + str(subTotalDonut))
+print(str(nbrOfRedBull) + " Red Bull(s) @ $2 each: $ " + str(subTotalRedBull))
+
+print("Tax at 6% = $" + str(taxAmount))
+
+print("*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*")    
+
+print("Total: $ " + str(grandTotal))
+
+

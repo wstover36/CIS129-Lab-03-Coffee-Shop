906b45e11de6283cc7d909d192bc201e1744d2c8
wstover36 <157067489+wstover36@users.noreply.github.com>
2024-02-29 11:40:50 -0700

    Create cis129_lab03_coffeeShop.py
    Started Programming Wolfgang's Coffee Shop 
    Added Americano, Dirty Chai Tea, Donuts in single for $1.00 and a dozen for $10.

---
+++ cis129_lab03_coffeeShop.py
@@ -0,0 +1,28 @@
+#Wolfgang C. Stover
+#CIS129
+
+print("*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@")
+print("*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@")
+
+print("Welcome to Wolfgang C. Stover's Coffee and Muffin Shop!")
+
+print("*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@")
+print("*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@*@‽*@‽*@")
+
+priceAmericano = 2.30
+priceDirtyChaiTea = 3.00
+priceDonut = 1.00
+priceDozenDonuts = 10
+
+totalPrice = priceAmericano + priceDirtyChaiTea
+
+tax = 0.09
+
+totalPriceWithTax = totalPrice + tax
+
+nbrOfDonuts = int(input("How many donuts would you like to buy?"))
+nbrOfAmericano = int(input("How many Americanos would you like to buy"))
+nbrOfDirtyChaiTea = int(input("How many dirty chai tea would you like to buy"))
+
+subTotal = (nbrOfDonuts * priceDonut)
+subTotal                           
